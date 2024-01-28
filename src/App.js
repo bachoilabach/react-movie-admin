594c95a72a957317f411749623143d5a87c86e4e
Nguyễn Việt Trường <112754228+Imtruongz@users.noreply.github.com>
2024-01-28 19:51:02 +0700

    Form Login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import SignupPage from './admin/pages/SignupPage';
+import LoginPage from './admin/pages/LoginPage';
 
 function App() {
   return (
-    <div>
-      <p className='font-bold text-center'>
-        Hello world!
-      </p>
+    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8 bg-blue-50">
+    <div className="max-w-md w-full space-y-8 p-4 bg-white rounded-2xl shadow-2xl">
+     <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>} />
+            <Route path="/signup" element={<SignupPage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
+  </div>
   );
 }
 
---
+++ src/admin/pages/LoginPage.js
@@ -0,0 +1,16 @@
+import Header from "../components/FormLogin/Header";
+import Login from "../components/FormLogin/Login";
+
+export default function LoginPage() {
+  return (
+    <>
+      <Header
+        heading="Login to your account"
+        paragraph="Don't have an account yet? "
+        linkName="Signup"
+        linkUrl="/signup"
+      />
+      <Login />
+    </>
+  );
+}
