f3341fbd2454ec50acc91e4230086eb9f7a7c858
Nguyễn Việt Trường <112754228+Imtruongz@users.noreply.github.com>
2024-03-06 14:00:46 +0700

    update statisticPage

--- src/main/components/Modal.jsx
+++ src/main/components/Modal.jsx
@@ -1,25 +1,19 @@
 import React, { useState } from "react";
 
-const Modal = ({ toggleModal }) => {
-  const [formData, setFormData] = useState({
-    movieId: "",
-    title: "",
-    genreId: "",
-    rating: "",
-    countryId: "",
-    release: "",
-    duration: "",
-    thumbnail: "",
-    videoURL: "",
-    html: "",
-  });
+import {
+  Input,
+  Popover,
+  PopoverHandler,
+  PopoverContent,
+} from "@material-tailwind/react";
 
-  const handleChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value,
-    });
-  };
+import { format } from "date-fns";
+import { DayPicker } from "react-day-picker";
+import { ChevronRightIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
+
+export default function Modal({ toggleModal }) {
+  const [date, setDate] = useState(Date);
+
   return (
     <div className="w-full h-[100vh] fixed inset-0 z-10">
       <div
@@ -27,175 +21,123 @@ const Modal = ({ toggleModal }) => {
         className="w-full h-full bg-black opacity-50"
       ></div>
       <div className="absolute w-11/12 h-5/6 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-gray-300 p-8 rounded overflow-scroll">
-        <form className="w-full p-4 bg-white rounded shadow-lg ">
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="movieId"
-            >
-              Movie ID
-            </label>
-            <input
-              type="text"
-              name="movieId"
-              value={formData.movieId}
-              onChange={handleChange}
-              placeholder="Enter Movie ID"
-              className="w-full p-2 border rounded-md"
+        <form className="w-full p-4 bg-white rounded shadow-lg">
+          <div className="mb-4 w-10">
+            <Input variant="standard" label="Movie ID" placeholder="Movie ID" />
+          </div>
+
+          <div className="mb-4 w-10">
+            <Input variant="standard" label="Title" placeholder="Title"/>
+          </div>
+
+          <div className="mb-4 w-10">
+            <Input
+              variant="standard"
+              label="Rating ID"
+              placeholder="Rating ID"
             />
           </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="title"
-            >
-              Title
-            </label>
-            <input
-              type="text"
-              name="title"
-              value={formData.title}
-              onChange={handleChange}
-              placeholder="Enter Title"
-              className="w-full p-2 border rounded-md"
+          <div className="mb-4 w-10">
+            <Input
+              variant="standard"
+              label="Country ID"
+              placeholder="Country ID"
             />
           </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Genre ID"
-            >
-              Genre ID
-            </label>
-            <input
-              type="text"
-              name="genreid"
-              value={formData.genreId}
-              onChange={handleChange}
-              placeholder="Enter Genre ID"
-              className="w-full p-2 border rounded-md"
+          <div className="mb-4 w-10">
+            <Popover placement="bottom">
+              <PopoverHandler>
+                <Input
+                variant="static"
+                  label="Select a Date"
+                  onChange={() => null}
+                  value={date ? format(date, "PPP") : ""}
+                />
+              </PopoverHandler>
+              <PopoverContent>
+                <DayPicker
+                  mode="single"
+                  selected={date}
+                  onSelect={setDate}
+                  showOutsideDays
+                  className="border-0"
+                  classNames={{
+                    caption:
+                      "flex justify-center py-2 mb-4 relative items-center",
+                    caption_label: "text-sm font-medium text-gray-900",
+                    nav: "flex items-center",
+                    nav_button:
+                      "h-6 w-6 bg-transparent hover:bg-blue-gray-50 p-1 rounded-md transition-colors duration-300",
+                    nav_button_previous: "absolute left-1.5",
+                    nav_button_next: "absolute right-1.5",
+                    table: "w-full border-collapse",
+                    head_row: "flex font-medium text-gray-900",
+                    head_cell: "m-0.5 w-9 font-normal text-sm",
+                    row: "flex w-full mt-2",
+                    cell: "text-gray-600 rounded-md h-9 w-9 text-center text-sm p-0 m-0.5 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-gray-900/20 [&:has([aria-selected].day-outside)]:text-white [&:has([aria-selected])]:bg-gray-900/50 first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+                    day: "h-9 w-9 p-0 font-normal",
+                    day_range_end: "day-range-end",
+                    day_selected:
+                      "rounded-md bg-gray-900 text-white hover:bg-gray-900 hover:text-white focus:bg-gray-900 focus:text-white",
+                    day_today: "rounded-md bg-gray-200 text-gray-900",
+                    day_outside:
+                      "day-outside text-gray-500 opacity-50 aria-selected:bg-gray-500 aria-selected:text-gray-900 aria-selected:bg-opacity-10",
+                    day_disabled: "text-gray-500 opacity-50",
+                    day_hidden: "invisible",
+                  }}
+                  components={{
+                    IconLeft: ({ ...props }) => (
+                      <ChevronLeftIcon
+                        {...props}
+                        className="h-4 w-4 stroke-2"
+                      />
+                    ),
+                    IconRight: ({ ...props }) => (
+                      <ChevronRightIcon
+                        {...props}
+                        className="h-4 w-4 stroke-2"
+                      />
+                    ),
+                  }}
+                />
+              </PopoverContent>
+            </Popover>
+          </div>
+
+          <div className="mb-4 w-10">
+            <Input
+              variant="standard"
+              label="Duration"
+              placeholder="duration"
             />
           </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Rating"
-            >
-              Rating
-            </label>
-            <input
-              type="text"
-              name="rating"
-              value={formData.rating}
-              onChange={handleChange}
-              placeholder="Enter Rating"
-              className="w-full p-2 border rounded-md"
+          <div className="mb-4 w-10">
+            <Input
+              variant="standard"
+              label="Thumbnail"
+              placeholder="thumbnail"
             />
           </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Country ID"
-            >
-              Country ID
-            </label>
-            <input
-              type="text"
-              name="countryID"
-              value={formData.countryId}
-              onChange={handleChange}
-              placeholder="Enter Country ID"
-              className="w-full p-2 border rounded-md"
+          <div className="mb-4 w-10">
+            <Input
+              variant="standard"
+              label="Video URL"
+              placeholder="videourl"
             />
           </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Release Date"
-            >
-              Release Date
-            </label>
-            <input
-              type="text"
-              name="ReleaseDate"
-              value={formData.release}
-              onChange={handleChange}
-              placeholder="Enter Release Date"
-              className="w-full p-2 border rounded-md"
+          <div className="mb-4 w-10">
+            <Input
+              variant="standard"
+              label="HTML"
+              placeholder="html"
             />
           </div>
 
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Duration"
-            >
-              Duration
-            </label>
-            <input
-              type="text"
-              name="duration"
-              value={formData.duration}
-              onChange={handleChange}
-              placeholder="Enter Duration"
-              className="w-full p-2 border rounded-md"
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Thumbnail URL"
-            >
-              Thumbnail URL
-            </label>
-            <input
-              type="text"
-              name="thumbnail"
-              value={formData.thumbnail}
-              onChange={handleChange}
-              placeholder="Enter Thumbnail URL"
-              className="w-full p-2 border rounded-md"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="Video URL"
-            >
-              Video URL
-            </label>
-            <input
-              type="text"
-              name="videoURL"
-              value={formData.videoURL}
-              onChange={handleChange}
-              placeholder="Enter Video URL"
-              className="w-full p-2 border rounded-md"
-            />
-          </div>
-
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="HTML Description"
-            >
-              HTML Description
-            </label>
-            <input
-              type="text"
-              name="html_description"
-              value={formData.html}
-              onChange={handleChange}
-              placeholder="Enter HTML Description"
-              className="w-full p-2 border rounded-md"
-            />
-          </div>
 
           <button
             type="submit"
@@ -213,6 +155,4 @@ const Modal = ({ toggleModal }) => {
       </button>
     </div>
   );
-};
-
-export default Modal;
+}
--- src/main/pages/CategoryPage.jsx
+++ src/main/pages/CategoryPage.jsx
@@ -5,13 +5,13 @@ import NavBar from "../components/NavBar";
 export default function CategoryPage() {
   return (
     <div className="flex bg-bgColorMain text-textMain p-[20px] gap-[3%]">
-			<SideBar />
-			<div className="w-full h-full flex flex-col gap-y-4">
-				<NavBar />
-				<div className=" bg-bgColorBlock rounded-lg h-[calc(100vh-136px)]">
-					<p>Category Page</p>					
-				</div>
-			</div>
-		</div>
+      <SideBar />
+      <div className="w-full h-full flex flex-col gap-y-4">
+        <NavBar />
+        <div className=" bg-bgColorBlock rounded-lg h-[calc(100vh-136px)]">
+          Content
+        </div>
+      </div>
+    </div>
   );
 }
