174a6ee7f764f6a3fc0f61ade60c9a7dd02c202f
bachoilabach <115871551+bachoilabach@users.noreply.github.com>
2024-05-16 11:31:17 +0700

    chore: add index for login path

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
       <BrowserRouter>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
+          <Route index path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/forgotpw" element={<ForgotPwPage />} />
           <Route path="/dashboard/*" element={<Dashboard />} />
